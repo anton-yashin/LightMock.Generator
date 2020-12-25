7685e5cce21b1f7f82d840603dca6c057b282dce
Anton Yashin <ktulhu@live.com>
2020-12-25 20:21:56 +0400

    dead code removed

--- src/LightMock.Generator.Tests/AbstractClass/BasicMethod.abstract.cs
+++ src/LightMock.Generator.Tests/AbstractClass/BasicMethod.abstract.cs
@@ -5,28 +5,4 @@
         protected abstract void DoSomething(int p);
         public abstract int GetSomething();
     }
-
-    //public interface IP2P_ABasicMethod
-    //{
-    //    void DoSomething(int p);
-    //}
-
-    //public partial class BasicMethod : ABasicMethod { }
-
-    //partial class BasicMethod : IP2P_ABasicMethod
-    //{
-    //    private readonly IInvocationContext<LightMock.Generator.Tests.AbstractClass.ABasicMethod> context;
-    //    private readonly IInvocationContext<IP2P_ABasicMethod> protectedContext;
-
-    //    public BasicMethod(IInvocationContext<LightMock.Generator.Tests.AbstractClass.ABasicMethod> context, IInvocationContext<IP2P_ABasicMethod> protectedContext)
-    //    {
-    //        this.context = context;
-    //        this.protectedContext = protectedContext;
-    //    }
-
-    //    void LightMock.Generator.Tests.AbstractClass.IP2P_ABasicMethod.DoSomething(int p) { protectedContext.Invoke(f => f.DoSomething(p)); }
-    //    override protected void DoSomething(int p) { protectedContext.Invoke(f => f.DoSomething(p)); }
-    //    override public int GetSomething() { return context.Invoke(f => f.GetSomething()); }
-
-    //}
 }
