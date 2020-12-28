f072d46bb2604a864963d71cb9973a47b60207e0
Anton Yashin <ktulhu@live.com>
2020-12-28 19:34:21 +0400

    ensure that non virtual, non abstract event not going to be overriden

--- src/LightMock.Generator.Tests/AbstractClass/EventSource.abstract.cs
+++ src/LightMock.Generator.Tests/AbstractClass/EventSource.abstract.cs
@@ -6,7 +6,12 @@ namespace LightMock.Generator.Tests.AbstractClass
     {
         public abstract event EventHandler<int>? OnPublicEvent;
         protected abstract event EventHandler<object>? OnProtectedEvent;
+#pragma warning disable CS0067 // Field not used
         public virtual event Action? OnPublicVirtualAction;
         protected virtual event Action? OnProtectedVirtualAction;
+
+        public event Action? NonVirtualNonAbstractPublicEvent;
+        protected event Action? NonVirtualNonAbstractProtectedEvent;
+#pragma warning restore CS0067 // Field not used
     }
 }
