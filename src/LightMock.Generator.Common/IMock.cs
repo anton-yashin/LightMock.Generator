88dfd2a576c2525745df4c53cc88eb9e692e380c
Anton Yashin <ktulhu@live.com>
2021-01-29 20:30:59 +0400

    refactor: reference mock object from IMock<T> interface

--- src/LightMock.Generator.Common/IMock.cs
+++ src/LightMock.Generator.Common/IMock.cs
@@ -5,6 +5,8 @@ namespace LightMock.Generator
     public interface IMock<T> : IMockContext<T>
         where T : class
     {
+        T Object { get; }
+
         void AssertGet<TResult>(Func<T, TResult> getterExpression);
         void AssertGet<TResult>(Func<T, TResult> getterExpression, Invoked invoked);
         void AssertSet(Action<T> setterExpression);
