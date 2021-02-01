fcb2e105fda549c748eb5c584c5975504b1fa3b7
Anton Yashin <ktulhu@live.com>
2021-02-01 19:33:49 +0400

    refactor: make GenerateMockAttribute obsolete

--- src/LightMock.Generator.Common/GenerateMockAttribute.cs
+++ src/LightMock.Generator.Common/GenerateMockAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+[Obsolete]
 public sealed class GenerateMockAttribute : Attribute
 {
     public GenerateMockAttribute() { }
