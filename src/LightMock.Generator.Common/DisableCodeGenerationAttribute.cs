5279ed25cd555974f146b6e2db9c4056cc1b2e9c
Anton Yashin <ktulhu@live.com>
2021-02-17 17:20:56 +0400

    docs: source code documentation

--- src/LightMock.Generator.Common/DisableCodeGenerationAttribute.cs
+++ src/LightMock.Generator.Common/DisableCodeGenerationAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace LightMock.Generator
 {
+    /// <summary>
+    /// Apply this attribute to your assembly to turn off this source code generator.
+    /// </summary>
     [AttributeUsage(validOn: AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
     public sealed class DisableCodeGenerationAttribute : Attribute
     {
