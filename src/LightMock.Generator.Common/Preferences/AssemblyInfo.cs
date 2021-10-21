2f54927abf54b11ff46f5288e9d94c900161f95f
Anton Iashin <ktulhu@live.com>
2021-10-21 17:35:50 +0400

    refactor: split projects

--- src/LightMock.Generator.Common/Preferences/AssemblyInfo.cs
+++ src/LightMock.Generator.Common/Preferences/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("LightMock.Generator")]
+[assembly:InternalsVisibleTo("LightMock.Generator.v40")]
 [assembly:InternalsVisibleTo("LightMock.Generator.Tests")]
+[assembly:InternalsVisibleTo("LightMock.Generator.Tests.Roslyn40")]
